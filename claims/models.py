df1e2dc9b5e054f973ba9186b13fef0dd77b4c34
Ali Salim <a.salim@SBG-MP.COM>
2020-12-15 14:51:34 +0300

    done for temp and for attachemnt too

--- claims/models.py
+++ claims/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+def image_upload (instance , filename):
+    imagename , extension = filename.split(".")
+    return "claims/%s/%s.%s"%(instance.id,instance.id,extension)
+
+STATUS = (
+    ('جديد','جديد'),
+    ('تحت الاصدار','تحت الاصدار'),
+    ('مرفوضه','مرفوضه'),
+)
+
+class Claim(models.Model):
+    empno = models.CharField( max_length=12 ,verbose_name='الرقم الوظيفي')
+    eiqama = models.CharField( max_length=10,verbose_name='هوية الموظف')
+    piqama = models.CharField( max_length=16 ,verbose_name='رقم بطاقةالمريض')
+    name = models.CharField( max_length=150 ,verbose_name='اسم المريض')
+    requester = models.CharField( max_length=25 ,verbose_name='مقدمة عن طريق') # should be username here.....
+    clinicname = models.CharField( max_length=150 ,verbose_name='العياده')
+    ramount = models.IntegerField(verbose_name='مبلغ المطالبة')
+    currency = models.CharField (max_length=25 ,verbose_name='العملة') # should be choices......
+    rdate = models.DateTimeField( auto_now=False, auto_now_add=True ,verbose_name='تاريخ الطلب')
+    note = models.CharField( max_length=500 ,verbose_name='ملاحظات')
+    status = models.CharField( max_length=50 ,verbose_name='الحالة' , choices=STATUS ,default='جديد')
+    image = models.ImageField( upload_to='claimsfile/', height_field=None, width_field=None, max_length=None, null = True , blank = True)
+
+    def __str__(self):
+        return str(self.id) +' - '+ self.piqama
--- claims/views.py
+++ claims/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Claim
 
 # Create your views here.
 
 def index(request):
+    allclaim = Claim.objects.all()
+    context = {'allclaim' : allclaim}
+
     
-    return render(request,'claims\index.html')
+    return render(request,'claims\index.html', context)
